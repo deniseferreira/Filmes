36b0910a632bf630ef4085a76a8bbd84ef332670
Denise <ferreiras.denise@hotmail.com>
2018-04-23 21:06:37 +0100

    rotas, forms e pipes

--- src/app/conteudo/conteudo.module.ts
+++ src/app/conteudo/conteudo.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
 
 
 import { ListaFimesComponent } from './lista-fimes/lista-fimes.component';
 import { FilmeCardComponent } from './filme-card/filme-card.component';
 
 import { FilmeService } from './filme.service';
+import { FormPesquisaComponent } from './lista-fimes/form-pesquisa/form-pesquisa.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   declarations: [
     ListaFimesComponent, 
-    FilmeCardComponent
+    FilmeCardComponent, FormPesquisaComponent
   ],
 
   exports: [ListaFimesComponent],
